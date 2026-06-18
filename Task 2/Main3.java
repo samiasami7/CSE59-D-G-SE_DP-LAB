e94af16aae919aa87fa0b48c88e5a18179897fbf
samiasami7 <samia082002@gmail.com>
2026-06-18 16:27:41 +0600

    Update Main3.java

--- Task 2/Main3.java
+++ Task 2/Main3.java
@@ -1,1 +1,17 @@
+class Countdown {
+  void start() {
+    for(int i=5; i>=1; i--) {
+      System.out.println(i);
+    }
+    System.out.println("GO!");
+  }
+}
 
+public class Main3 {
+
+  public static void main(String[] args)
+  {
+    Countdown cd = new Countdown();
+    cd.start();
+  }
+}
