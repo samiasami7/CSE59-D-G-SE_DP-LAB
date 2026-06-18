e5a5f99ea935f1d1e6205245baea6e0250433708
samiasami7 <samia082002@gmail.com>
2026-06-18 16:44:58 +0600

    Update Main7.java

--- Task 2/Main7.java
+++ Task 2/Main7.java
@@ -1,1 +1,18 @@
+interface Clickable {
+  void onClick();
+}
 
+class Button implements Clickable {
+  @Override
+  public void onClick() {
+    System.out.println("Button was clicked!");
+  }
+}
+
+public class Main7 {
+  public static void main(String[] args)
+   {
+    Button button = new Button();
+    button.onClick();
+   }
+}
