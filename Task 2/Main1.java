c39e10054ce29f000b09603bbdf7b9cc94ceeb53
samiasami7 <samia082002@gmail.com>
2026-06-18 16:00:21 +0600

    Update Main1.java

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,18 @@
 
+class ContestStats {
+
+    int[] scores ={85,90,95};
+
+    void printFristScore() {
+        System.out.println(scores[0]);
+
+    }
+}
+
+public class Main1 {
+
+        public static void main(String[] args) {
+            ContestStats stats = new ContestStats();
+            stats.printFristScore();
+        }
+}
