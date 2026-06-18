d693bcb11a583318a811a785fe12bd97f7651d9d
samiasami7 <samia082002@gmail.com>
2026-06-18 16:37:11 +0600

    Update Main5.java

--- Task 2/Main5.java
+++ Task 2/Main5.java
@@ -1,1 +1,22 @@
+class Printer {
 
+  void printdata(String text) {
+    System.out.println("Text: "+text);
+
+  }
+
+  void printdata(int number) {
+    System.out.println("Number: "+number);
+  }
+}
+
+public class Main5 {
+
+  public static void main(String[] args){
+
+    Printer printer = new Printer();
+
+    printer.printdata("Hello, World!");
+    printer.printdata(100);
+  }
+}
