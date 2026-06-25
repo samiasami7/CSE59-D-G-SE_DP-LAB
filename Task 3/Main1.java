1bd47f575bb3538f26cb199e6554ff56c5b3bab6
samiasami7 <samia082002@gmail.com>
2026-06-25 17:00:04 +0600

    Update Main1.java

--- Task 3/Main1.java
+++ Task 3/Main1.java
@@ -1,1 +1,53 @@
+public class Main {
 
+    public static void main(String[] args) {
+
+        User user = new User("john_deo", "john@example.com");
+        
+        if(EmailValidator.isValid(user.getEmail())) {
+          UserRespository respository = new UserRespository();
+          respository.save(user);
+        }
+    }
+}
+
+class User {
+  private String username;
+  private String email;
+
+  public User(String username, String email) {
+    this.username = username;
+    this.email = email;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
+
+class EmailValidator {
+
+  public static boolean isValid(String email) {
+    if (email !=null && email.contains("@")) {
+      return true;
+    }
+    System.out.println("Invalid email format.");
+    return false;
+
+  }
+}
+
+class UserRespository {
+    
+    public void save(User user) {
+      System.out.println("Connecting to database.......");
+      System.out.println(
+        "Saving user " +user.getUsername() + "to the user table."
+      );
+    
+    }
+}
