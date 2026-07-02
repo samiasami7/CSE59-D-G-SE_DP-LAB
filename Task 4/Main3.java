38667650905bbc3fc82ca9e07d5216ba2fea528b
samiasami7 <samia082002@gmail.com>
2026-07-02 17:27:07 +0600

    Update Main3.java

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,46 @@
+interface MessageSender {
+	void sendMessage(String message);
+}
+
+class EmailSender implements MessageSender {
+	
+	@Override
+	public void sendMessage(String message ) {
+		System.out.println("Sending Email: "+message);
+	}
+}
+
+class SMSSender implements MessageSender {
+	
+	@Override
+	public void sendMessage(String message ) {
+		System.out.println("Sending SMS: "+message);
+	}
+}
+
+class NotificationService {
+	private MessageSender messageSender;
+	
+	public NotificationService(MessageSender messageSender) {
+		this.messageSender = messageSender;
+	}
+	
+	public void alterUser(String msg) {
+		messageSender.sendMessage(msg);
+	}
+}
+
+
+public class Main3 {
+	public static void main(String[] args) {
+		
+		MessageSender email = new EmailSender();
+		NotificationService notic1 = new NotificationService (email);
+		notic1.alterUser("Your order has been shipped!");
+		
+		MessageSender sms = new SMSSender();
+		NotificationService notic2 = new NotificationService(sms);
+		notic2.alterUser("Your OTP is 232115273.");
+	}
+}
 
